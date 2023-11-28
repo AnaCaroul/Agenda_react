54f6c4b3bdc9cdebb35024e3b176a711e711e39f
Estevão Trad Gago <12102792@aluno.cotemig.com.br>
2023-11-27 21:51:31 -0300

    Balalaika
    AAAAAAAAAAAAAAAAA

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import React from "react";
 import { Box } from './componentes/Boxs/Box';
 import { Header } from './componentes/header/header';
-import { Spacer  } from './style';
+import { Spacer } from './style';
 import { BoxTime } from './componentes/BoxTime/BoxTime';
 import { BoxTeacher } from './componentes/BoxTeacher/BoxTeacher';
 
 function App() {
-   // Array para armazenar os valores de input de cada Box
-   const [boxValues, setBoxValues] = React.useState([]);
+  // Array para armazenar os valores de input de cada Box
+  const [boxValues, setBoxValues] = React.useState([]);
 
   // Função para lidar com o valor do input quando o botão é clicado
   const handleBoxButtonClick = (index, value) => {
@@ -19,11 +19,9 @@ function App() {
       return newValues;
     });
 
+    // Faça algo com os valores de input no contexto do App
     console.log("Valores de Input:", boxValues);
-    // Faça algo com os valores de input no contexto do Appa
   };
-
-
 
   // Array para armazenar os valores de input de cada BoxTime
   const [boxTimeValues, setBoxTimeValues] = React.useState([]);
@@ -33,57 +31,54 @@ function App() {
     // Atualiza o array de valores com o valor específico de BoxTime
     setBoxTimeValues((prevValues) => [...prevValues, value]);
 
-    console.log("Valores de Input em App.js:", boxTimeValues);
     // Faça algo com os valores de input no contexto do App
+    console.log("Valores de Input em App.js:", boxTimeValues);
   };
-
 
   const dynamicOptions = ["Opção 1", "Opção 2", "Opção 3"];
 
-
   return (
     <>
-    <Header />
-    <Spacer margin="5%" />
-    <div className="conteiner"> 
-      <Box 
-      key={0}
-      text={`Cadastro de Professores`}
-      placeholder={`Digite o nome do professor...`}
-      onButtonClick={(value) => handleBoxButtonClick(0, value)}
-      />
+      <Header />
+      <Spacer margin="5%" />
+      <div className="container">
+        <Box
+          key={0}
+          text={`Cadastro de Professores`}
+          placeholder={`Digite o nome do professor...`}
+          onButtonClick={(value) => handleBoxButtonClick(0, value)}
+        />
 
-      <Box 
-      key={1}
-      text={`Cadastro de Disciplina`}
-      placeholder={`Digite o nome da disciplina...`}
-      onButtonClick={(value) => handleBoxButtonClick(1, value)}
-      /> 
+        <Box
+          key={1}
+          text={`Cadastro de Disciplina`}
+          placeholder={`Digite o nome da disciplina...`}
+          onButtonClick={(value) => handleBoxButtonClick(1, value)}
+        />
 
-      <Box 
-      key={2}
-      text={`Cadastro de Turma`}
-      placeholder={`Digite o nome da Turma...`}
-      onButtonClick={(value) => handleBoxButtonClick(2, value)}
-      /> 
-    </div>
+        <Box
+          key={2}
+          text={`Cadastro de Turma`}
+          placeholder={`Digite o nome da Turma...`}
+          onButtonClick={(value) => handleBoxButtonClick(2, value)}
+        />
+      </div>
 
-    <Spacer margin="2%"/>
+      <Spacer margin="2%" />
 
-    <div className='conteiner2'> 
-        <BoxTime 
+      <div className='container2'>
+        <BoxTime
           placeholder="Digite o número de dias..."
           onButtonClick={handleBoxTimeButtonClick}
           options={dynamicOptions}
-        />   
-    </div>
+        />
+      </div>
 
-    <Spacer margin="2%"/>
+      <Spacer margin="2%" />
 
-    <div className='conteiner2'> 
-      <BoxTeacher />
-    </div>
-    
+      <div className='container2'>
+        <BoxTeacher />
+      </div>
     </>
   );
 }
--- src/componentes/Boxs/Box.js
+++ src/componentes/Boxs/Box.js
@@ -1,43 +1,39 @@
-import { Button, Tipografia } from "../../style";
-import "./BoxS";
 import React, { useState } from "react";
-import { Div, Input } from "./BoxS";
+import { Div, Input, Button, Typography } from "./BoxS"; // Certifique-se de importar os componentes corretos
 
+export const Box = (props) => {
+  const { text, placeholder, onButtonClick } = props;
 
-export const Box = (props) =>  {
+  // Estado local para armazenar o valor do input
+  const [inputValue, setInputValue] = useState("");
 
-    const { text, placeholder, onButtonClick } = props
+  // Manipulador de evento para atualizar o estado quando o valor do input muda
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-    // Estado local para armazenar o valor do input
-    const [inputValue, setInputValue] = useState("");
+  // Função para lidar com o clique no botão e chamar a função de retorno de chamada
+  const handleButtonClick = () => {
+    // Chama a função de retorno de chamada passando o valor do input
+    onButtonClick(inputValue);
 
-    // Manipulador de evento para atualizar o estado quando o valor do input muda
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
-    };
+    // Limpa o valor do input após o clique no botão
+    setInputValue("");
+  };
 
-    // Função para lidar com o clique no botão e chamar a função de retorno de chamada
-    const handleButtonClick = () => {
-        // Chama a função de retorno de chamada passando o valor do input
-        onButtonClick(inputValue);
+  return (
+    <>
+      <Div>
+        <Typography size="22px">{text}</Typography>
 
-        setInputValue("");
-    };
-    
+        <Input
+          placeholder={placeholder}
+          value={inputValue}
+          onChange={handleInputChange}
+        />
 
-    return(
-        <>
-           <Div> 
-            <Tipografia size="22px">{text}</Tipografia>
-
-            <Input 
-            placeholder={placeholder} 
-            value={inputValue}
-            onChange={handleInputChange}
-            />
-
-            <Button onClick={handleButtonClick}> CADASTRAR </Button>
-            </Div> 
-        </>
-    )
-;}
+        <Button onClick={handleButtonClick}>CADASTRAR</Button>
+      </Div>
+    </>
+  );
+};
